c06330b245df7089d3d9163206c23a49335649e4
yarenbayirr <yaren.bayir96@gmail.com>
2023-03-08 16:05:14 +0300

    Final Rev

--- app/src/app/models/user.ts
+++ app/src/app/models/user.ts
@@ -3,15 +3,15 @@ export interface User {
     name: string;
     username: string;
     email: string;
-    address: [
+    address: {
         street: string,
         suite: string,
         city: string,
         zipcode: string,
-        geo: [lat: string, lng:string]
-    ]
+        geo: {lat: string, lng:string}
+    }
     
     phone: string;
     website: string;
-    company: [name: string, catchPhrase: string, bs: string]
+    company: {name: string, catchPhrase: string, bs: string}
 }
